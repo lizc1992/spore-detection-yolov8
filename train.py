e2bd4929135cf481cedcf23d8798db5f8ff4197a
lizc1992 <31059707+lizc1992@users.noreply.github.com>
2025-10-25 21:38:19 +0300

    Create train.py

---
+++ train.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8m-seg.pt")
+
+# Train
+model.train(
+    data="cells_aug.yaml",
+    epochs=200,
+    imgsz=640,
+    patience=20,
+    batch=8,
+    augment=True,
+    project="spore_yolov8m_aug",
+    name="cells",
+)
